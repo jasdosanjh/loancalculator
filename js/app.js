8285fdb4daed21dca2ca2a981faf6b03c69862ad
jazdosanjh <jaspinder@live.co.uk>
2019-01-10 18:27:28 +0000

    Created the loader for the application

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    // Hide Results
+    document.getElementById('results').style.display = 'none';
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
 
 // Calculate Results
 function calculateResults(e) {
@@ -11,28 +18,32 @@ function calculateResults(e) {
     const monthlyPayment = document.getElementById('monthly-payment');
     const totalPayment = document.getElementById('total-payment');
     const totalInterest = document.getElementById('total-interest');
-
     const principal = parseFloat(amount.value);
     const calculatedInterest = parseFloat(interest.value) / 100 / 12;
     const calculatedPayments = parseFloat(years.value) * 12;
-
     // Compute monthly payment
     const x = Math.pow(1 + calculatedInterest, calculatedPayments);
     const monthly = (principal * x * calculatedInterest)/(x - 1);
-
     if (isFinite(monthly)) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        // Show Results
+        document.getElementById('results').style.display = 'block';
+        // Hide Loader
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check  your numbers.');
     }
-
     e.preventDefault();
 }
 
 // Show Error Message
 function showError(error) {
+    // Show Results
+    document.getElementById('results').style.display = 'none';
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none';
     // Create a div
     const errorDiv = document.createElement('div');
     // Get elements
